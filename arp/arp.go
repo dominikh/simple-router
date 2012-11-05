283876cb15b54f0950ec6af29923e5e8ea813c51
Dominik Honnef <dominikh@fork-bomb.org>
2012-11-05 06:22:42 +0100

    parse /proc/net/arp instead of using the arp binary

--- arp/arp.go
+++ arp/arp.go
@@ -1,34 +1,31 @@
 package arp
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net"
-	"os/exec"
-	"regexp"
-	"strings"
 )
-
-// TODO allow different interfaces
 
 type Entry struct {
 	IP              net.IP
 	HardwareAddress net.HardwareAddr
 }
 
-var re = regexp.MustCompile("^\\S+ \\((.+?)\\) at (\\S+)")
-
 func ARP() ([]Entry, error) {
 	entries := make([]Entry, 0)
 
-	output, err := exec.Command("/usr/sbin/arp", "-a", "-n", "-i", "eth1").Output()
+	data, err := ioutil.ReadFile("/proc/net/arp")
 	if err != nil {
 		return entries, err
 	}
 
-	lines := strings.Split(strings.TrimSpace(string(output)), "\n")
-	for _, line := range lines {
-		matches := re.FindStringSubmatch(line)
-		mac, _ := net.ParseMAC(matches[2])
-		entries = append(entries, Entry{net.ParseIP(matches[1]), mac})
+	lines := bytes.Split(bytes.TrimSpace(data), []byte("\n"))
+	for _, line := range lines[1:] {
+		fields := bytes.Fields(line)
+		ip := fields[0]
+		hw := fields[3]
+		mac, _ := net.ParseMAC(string(hw))
+		entries = append(entries, Entry{net.ParseIP(string(ip)), mac})
 	}
 
 	return entries, nil
