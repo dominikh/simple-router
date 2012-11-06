0f4c57bfa115bade92c6fa1cb896d2f7d116ab3d
Dominik Honnef <dominikh@fork-bomb.org>
2012-11-06 19:58:14 +0100

    conntrack: change filter API

--- web/web.go
+++ web/web.go
@@ -134,7 +134,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func natJsonHandler(w http.ResponseWriter, r *http.Request) {
-	natEntries := getConntrackFlows().Filter(conntrack.SNATFilter|conntrack.DNATFilter)
+	natEntries := getConntrackFlows().FilterByType(conntrack.SNATFilter|conntrack.DNATFilter)
 	natEntriesDumbedDown := make([]NATEntry, len(natEntries))
 
 	for index, entry := range natEntries {
