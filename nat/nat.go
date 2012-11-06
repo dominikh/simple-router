2085ae090a4140a6777f7f705b747a431a9a8858
Dominik Honnef <dominikh@fork-bomb.org>
2012-11-06 06:36:55 +0100

    replace old NAT handling (parsing netstat-nat output) with own NAT parsing

--- nat/nat.go
+++ nat/nat.go
@@ -1,79 +1,127 @@
 package nat
 
 import (
+	"github.com/dominikh/simple-router/conntrack"
+
 	"net"
-	"os/exec"
-	"strings"
 )
 
-type Entry struct {
-	Protocol           string
-	SourceAddress      string
-	SourcePort         string
-	DestinationAddress string
-	DestinationPort    string
-	State              string
+type Flag uint8
+
+const (
+	SNAT Flag = 1 << iota
+	DNAT
+	Routed
+	Local
+)
+
+var localIPs = make([]*net.IPNet, 0)
+
+func isLocalIP(ip net.IP) bool {
+	for _, localIP := range localIPs {
+		if localIP.IP.Equal(ip) {
+			return true
+		}
+	}
+
+	return false
 }
 
-func GetDNAT(resolve bool) ([]Entry, error) {
-	return getNAT("-D", resolve)
-}
-
-func GetSNAT(resolve bool) ([]Entry, error) {
-	return getNAT("-S", resolve)
-}
-
-func GetNAT(resolve bool) ([]Entry, error) {
-	return getNAT("", resolve)
-}
-
-func getNAT(flag string, resolve bool) ([]Entry, error){
-	output, err := exec.Command("sudo", "/usr/bin/netstat-nat", "-o", "-n", flag).Output()
+func init() {
+	addresses, err := net.InterfaceAddrs()
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
 
-	return parseNetstat(string(output), resolve), nil
+	for _, address := range addresses {
+		localIPs = append(localIPs, address.(*net.IPNet))
+	}
 }
 
-func parseNetstat(input string, resolve bool) []Entry {
-	entries := make([]Entry, 0)
+func GetSNAT(flows []conntrack.Flow) []conntrack.Flow {
+	return GetNAT(flows, SNAT)
+}
 
-	lines := strings.Split(strings.TrimSpace(input), "\n")
-	for _, line := range lines {
-		line = strings.TrimSpace(line)
-		fields := strings.Fields(line)
-		if len(fields) < 4 {
-			continue
+func GetDNAT(flows []conntrack.Flow) []conntrack.Flow {
+	return GetNAT(flows, DNAT)
+}
+
+func GetRouted(flows []conntrack.Flow) []conntrack.Flow {
+	return GetNAT(flows, Routed)
+}
+
+func GetLocal(flows []conntrack.Flow) []conntrack.Flow {
+	return GetNAT(flows, Local)
+}
+
+func GetNAT(flows []conntrack.Flow, which Flag) []conntrack.Flow {
+	natFlows := make([]conntrack.Flow, 0, len(flows))
+
+	snat := (which & SNAT) > 0
+	dnat := (which & DNAT) > 0
+	local := (which & Local) > 0
+	routed := (which & Routed) > 0
+
+	for _, flow := range flows {
+		if (snat && isSNAT(flow)) ||
+			(dnat && isDNAT(flow)) ||
+			(local && isLocal(flow)) ||
+			(routed && isRouted(flow)) {
+
+			natFlows = append(natFlows, flow)
 		}
-		protocol := fields[0]
-		source := fields[1]
-		destination := fields[2]
-		state := fields[3]
-
-		sourceParts := strings.Split(source, ":")
-		destinationParts := strings.Split(destination, ":")
-
-		sourceAddress := sourceParts[0]
-		sourcePort := sourceParts[1]
-
-		destinationAddress := destinationParts[0]
-		destinationPort := destinationParts[1]
-
-		if resolve {
-			lookup, err := net.LookupAddr(destinationAddress)
-			if err == nil && len(lookup) > 0 {
-				destinationAddress = lookup[0]
-			}
-
-			lookup, err = net.LookupAddr(sourceAddress)
-			if err == nil && len(lookup) > 0 {
-				sourceAddress = lookup[0]
-			}
-		}
-
-		entries = append(entries, Entry{protocol, sourceAddress, sourcePort, destinationAddress, destinationPort, state})
 	}
 
-	return entries
+	return natFlows
+}
+
+func isSNAT(flow conntrack.Flow) bool {
+	// SNATed flows should reply to our WAN IP, not a LAN IP.
+	if flow.Original.Source.Equal(flow.Reply.Destination) {
+		return false
+	}
+
+	if !flow.Original.Destination.Equal(flow.Reply.Source) {
+		return false
+	}
+
+	return true
+}
+
+func isDNAT(flow conntrack.Flow) bool {
+	// Reply must go back to the source; Reply mustn't come from the WAN IP
+	if flow.Original.Source.Equal(flow.Reply.Destination) && !flow.Original.Destination.Equal(flow.Reply.Source) {
+		return true
+	}
+
+	// Taken straight from original netstat-nat, labelled "DNAT (1 interface)"
+	if !flow.Original.Source.Equal(flow.Reply.Source) && !flow.Original.Source.Equal(flow.Reply.Destination) && !flow.Original.Destination.Equal(flow.Reply.Source) && flow.Original.Destination.Equal(flow.Reply.Destination) {
+		return true
+	}
+
+	return false
+}
+
+func isLocal(flow conntrack.Flow) bool {
+	// no NAT
+	if flow.Original.Source.Equal(flow.Reply.Destination) && flow.Original.Destination.Equal(flow.Reply.Source) {
+		// At least one local address
+		if isLocalIP(flow.Original.Source) || isLocalIP(flow.Original.Destination) || isLocalIP(flow.Reply.Source) || isLocalIP(flow.Reply.Destination) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func isRouted(flow conntrack.Flow) bool {
+	// no NAT
+	if flow.Original.Source.Equal(flow.Reply.Destination) && flow.Original.Destination.Equal(flow.Reply.Source) {
+		// No local addresses
+		if !isLocalIP(flow.Original.Source) && !isLocalIP(flow.Original.Destination) && !isLocalIP(flow.Reply.Source) && !isLocalIP(flow.Reply.Destination) {
+			return true
+		}
+	}
+
+	return false
 }
