31b3632e5e8e01c73ba5d7456a4d16db59ecc110
Dominik Honnef <dominikh@fork-bomb.org>
2012-11-07 04:42:05 +0100

    netdb: add GetServByName and GetServByPort

--- netdb/netdb.go
+++ netdb/netdb.go
@@ -13,7 +13,15 @@ type Protoent struct {
 	Number  int
 }
 
+type Servent struct {
+	Name     string
+	Aliases  []string
+	Port     int
+	Protocol string
+}
+
 var Protocols []Protoent
+var Services []Servent
 
 func init() {
 	// Load protocols
@@ -26,7 +34,7 @@ func init() {
 		line = strings.TrimSpace(line)
 		split := strings.SplitN(line, "#", 2)
 		fields := strings.Fields(split[0])
-		if len(fields) != 3 {
+		if len(fields) < 3 {
 			continue
 		}
 
@@ -37,8 +45,40 @@ func init() {
 
 		Protocols = append(Protocols, Protoent{
 			Name:    fields[0],
-			Aliases: strings.Fields(fields[2]),
+			Aliases: fields[2:],
 			Number:  int(num),
+		})
+	}
+
+	// Load services
+	data, err = ioutil.ReadFile("/etc/services")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, line := range strings.Split(string(data), "\n") {
+		line = strings.TrimSpace(line)
+		split := strings.SplitN(line, "#", 2)
+		fields := strings.Fields(split[0])
+		if len(fields) < 3 {
+			continue
+		}
+
+		name := fields[0]
+		portproto := strings.SplitN(fields[1], "/", 2)
+		port, err := strconv.ParseInt(portproto[0], 10, 32)
+		if err != nil {
+			panic(err)
+		}
+
+		proto := portproto[1]
+		aliases := fields[2:]
+
+		Services = append(Services, Servent{
+			Name:     name,
+			Aliases:  aliases,
+			Port:     int(port),
+			Protocol: proto,
 		})
 	}
 }
@@ -70,4 +110,34 @@ func GetProtoByName(name string) (Protoent, error) {
 	}
 
 	return Protoent{}, errors.New("Unknown protocol number")
+}
+
+func GetServByName(name, protocol string) (Servent, error) {
+	for _, servent := range Services {
+		if servent.Protocol != protocol && protocol != "" {
+			continue
+		}
+
+		if servent.Name == name {
+			return servent, nil
+		}
+
+		for _, alias := range servent.Aliases {
+			if alias == name {
+				return servent, nil
+			}
+		}
+	}
+
+	return Servent{}, errors.New("Unknown service name/protocol combination")
+}
+
+func GetServByPort(port int, protocol string) (Servent, error) {
+	for _, servent := range Services {
+		if servent.Port == port && (servent.Protocol == protocol || protocol == "") {
+			return servent, nil
+		}
+	}
+
+	return Servent{}, errors.New("Unknown port/protocol combination")
 }
