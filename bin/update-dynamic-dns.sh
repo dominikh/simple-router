92e945cdbf054d28b31f3906423ff08265ca4837
Dominik Honnef <dominikh@fork-bomb.org>
2013-01-01 17:35:14 +0100

    delete update-dynamic-dns.sh because in its current state it contains sensitive information
    Don't bother, the API key has been changed since.

--- bin/update-dynamic-dns.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-LINODE_API_KEY="rY0CbkBA83OGLDYW9t965qbp9ooc39AyUhGG6ZAM3SMjhUDccpgNET6eFs5PdgC2"
-LINODE_DOMAIN_ID="179075"
-LINODE_RESOURCE_ID="2438303"
-
-wget "https://api.linode.com/?api_key=${LINODE_API_KEY}&api_action=domain.resource.update&DomainID=${LINODE_DOMAIN_ID}&ResourceID=${LINODE_RESOURCE_ID}&Target=[remote_addr]" -O - &> /dev/null
